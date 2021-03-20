36d95ef6d4de6180adc825367d6a98a8728b45a1
Yusuf Adanur <y.adanur@gmail.com>
2021-03-20 14:24:01 -0700

    Completed all requirements

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var currentRotation = 0;
+var isMoving = 0;
+var timeOutId;
 
 function turnLeft(X) {
   var image = document.getElementById('race-car');
@@ -12,11 +14,40 @@ function turnRight(X) {
 
 document.onkeydown = function (e) {
   if (e.key === 'ArrowLeft') {
-    currentRotation -= 90;
+    currentRotation = (currentRotation - 90) % 360; // Decrease the degree while preventing the value go less than -360 degrees
     turnLeft(currentRotation);
   }
   if (e.key === 'ArrowRight') {
-    currentRotation += 90;
+    currentRotation = (currentRotation + 90) % 360; // Increase the degree while preventing the value go over 360 degrees
     turnRight(currentRotation);
   }
+  // Stop and Restart the car
+  if (e.key === 'ArrowDown' && isMoving === 1) {
+    clearInterval(timeOutId);
+    isMoving = 0;
+  }
+  if (e.key === 'ArrowUp' && isMoving === 0) {
+    timeOutId = window.setInterval(moveCar, 10);
+    isMoving = 1;
+  }
 };
+
+function moveCar() {
+  isMoving = 1;
+  var x = document.getElementById('race-car').offsetLeft;
+  var y = document.getElementById('race-car').offsetTop;
+
+  if (currentRotation === 0) { // Facing to the right
+    x += 1;
+    document.getElementById('race-car').style.left = x + 'px';
+  } else if (currentRotation === 90 || currentRotation === -270) { // Facing down
+    y += 1;
+    document.getElementById('race-car').style.top = y + 'px';
+  } else if (currentRotation === 180 || currentRotation === -180) { // Facing to the left
+    x -= 1;
+    document.getElementById('race-car').style.left = x + 'px';
+  } else if (currentRotation === 270 || currentRotation === -90) { // Facing up
+    y -= 1;
+    document.getElementById('race-car').style.top = y + 'px';
+  }
+}
